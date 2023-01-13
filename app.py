3f6010749b1dca235768ae0b0c20cc47990bf424
thuliofmcarvalho@gmail.com <Lenovosl400intel>
2023-01-13 12:02:58 -0300

    Renomeado aplicação principal

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__) # name faz referencia ao proprio arquivo e garante que vai rodar a aplicação
 app.config.from_pyfile('config.py') #puxando as configurações do arquivo config
@@ -9,10 +10,11 @@ app.config.from_pyfile('config.py') #puxando as configurações do arquivo confi
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 bcrypt = Bcrypt(app)
+cors = CORS(app)
 
 #executando as rotas
 from rotas_game import *
 from rotas_usuarios import *
 
 if __name__ == '__main__':
-    app.run(debug=True) # faz rodar a aplicação
+    app.run() # faz rodar a aplicação
