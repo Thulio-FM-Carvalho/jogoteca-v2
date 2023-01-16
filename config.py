a26417ea5fd6f1a4fe7b8ea42a0906bfdec30e09
thuliofmcarvalho@gmail.com <Lenovosl400intel>
2023-01-16 11:06:22 -0300

    Alterado conexão do banco de dados

--- config.py
+++ config.py
@@ -2,14 +2,15 @@ import os
 
 SECRET_KEY = 'alura' # adicionando camada de criptografia no site
 
+SQLALCHEMY_DATABASE_URI = "postgresql://jogotecav2_postgres_user:Xl8cHnFJLYxRB1GUFKOI9jkZ0twVLCLS@dpg-cf26p5mn6mpkr6dh3hu0-a.oregon-postgres.render.com/jogotecav2_postgres"
 #fazendo a conexão com o banco de dados
-SQLALCHEMY_DATABASE_URI = \
-    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD = 'mysql+mysqlconnector',
-        usuario = 'root',
-        senha = 'root',
-        servidor = 'localhost',
-        database = 'jogoteca'
-    )
+# SQLALCHEMY_DATABASE_URI = \
+    #'{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        #SGBD = 'mysql+mysqlconnector',
+        #usuario = 'root',
+        #senha = 'root',
+        #servidor = 'localhost',
+        #database = 'jogoteca'
+    #)
 
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
